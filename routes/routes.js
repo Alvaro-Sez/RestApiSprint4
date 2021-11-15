f02d72017adc44453633234d134e665d71fab777
Alvaro <arrovaalvaro@gmail.com>
2021-11-15 13:01:13 +0100

    acabado

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,13 @@
 const 
 express = require('express'),
 routes = express.Router(),
+cors = require('cors'),
 uploadImgs = require('../libs/multerConfig')
 
+
+/* Nivell 1 */
+
+/* exercici 1  */
 routes.get('/user',(req,res)=>{
   res
     .status(200)
@@ -13,6 +18,7 @@ routes.get('/user',(req,res)=>{
     })
 })
 
+/* exercici 2  */
 routes.post('/upload',uploadImgs.single('image'), (req,res)=>{
   if(req.errorValidation){
     res.status(400).send({message: req.errorValidation})
@@ -21,4 +27,26 @@ routes.post('/upload',uploadImgs.single('image'), (req,res)=>{
   
 })
 
+/* Nivell 2 */
+
+/* exercici 1 */
+const cacheMiddleware = (req,res,next)=>{
+  res.set('Cache-control', 'no-cache')
+  next()
+}
+const checkAuth = (req,res,next)=>{
+  if(req.headers.authorization){
+    next()
+  } else {
+    res.status(401).send({message: 'Unauthorized'})
+  }
+
+}
+
+routes.post('/time', cors(), checkAuth, cacheMiddleware, (req,res)=>{
+  dateNow = {
+    date: new Date().toLocaleString()
+  }
+  res.status(200).send({dateNow})
+})
 module.exports = routes
